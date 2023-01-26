ff8c47907e441ea9b25591d698917659e12f480a
trteel1 <tateelamino@gmail.com>
2023-01-26 19:58:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Abdo is the champion';
